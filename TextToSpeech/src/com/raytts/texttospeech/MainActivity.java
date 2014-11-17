e63ff1051e2570f5ef3716fb2fa049dc3b35f930
ray <rayzhang415@gmail.com>
2014-11-16 23:56:10 -0800

    Added couple text-to-speech buttons

--- TextToSpeech/src/com/raytts/texttospeech/MainActivity.java
+++ TextToSpeech/src/com/raytts/texttospeech/MainActivity.java
@@ -9,20 +9,21 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
-    TextToSpeech ttspeech;
-    private EditText write;
+    private TextToSpeech ttspeech;
+    private EditText editText;
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        write = (EditText) findViewById(R.id.editText1);
+        
+        editText = (EditText) findViewById(R.id.editText);
         ttspeech = new TextToSpeech(getApplicationContext(), 
             new TextToSpeech.OnInitListener() {
                 @Override
                 public void onInit(int status) {
                     if (status != TextToSpeech.ERROR) {
-                        ttspeech.setLanguage(Locale.UK);
+                        ttspeech.setLanguage(Locale.US);
                     }
                 }
             });
@@ -38,8 +39,28 @@ public class MainActivity extends Activity {
     }
     
     public void speakText(View view) {
-       String toSpeak = write.getText().toString();
+       String toSpeak = editText.getText().toString();
        Toast.makeText(getApplicationContext(), toSpeak, Toast.LENGTH_SHORT).show();
        ttspeech.speak(toSpeak, TextToSpeech.QUEUE_FLUSH, null);
+    }
+    
+    public void speakYes(View view) {
+        String str = "Yes";
+        ttspeech.speak(str, TextToSpeech.QUEUE_FLUSH, null);
+    }
+    
+    public void speakNo(View view) {
+        String str = "No";
+        ttspeech.speak(str, TextToSpeech.QUEUE_FLUSH, null);
+    }
+    
+    public void speakTY(View view) {
+        String str = "Thank you";
+        ttspeech.speak(str, TextToSpeech.QUEUE_FLUSH, null);
+    }
+    
+    public void speakYW(View view) {
+        String str = "You're welcome";
+        ttspeech.speak(str, TextToSpeech.QUEUE_FLUSH, null);
     }
 }
